3e06a0024fc7812ff60327b361d7c4b02131c2c8
Oleg Kotsubko <olegkotsubko@gmail.com>
2021-05-25 17:12:05 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+const gulp = require("gulp");
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const del = require('del');
+
+const config = {
+    src: './src',
+    dest: './build'
+}
+
+gulp.task('copy:img', () => gulp
+  .src([`${config.src}/**/*.{jpg,png,jpeg}`])
+  .pipe(imagemin([
+    imagemin.mozjpeg({quality: 70, progressive: true}),
+    imagemin.optipng({optimizationLevel: 5}),
+  ]))
+  .pipe(gulp.dest(config.dest))
+);
+
+gulp.task('copy:webp', () => gulp
+  .src([`${config.src}/**/*.{jpg,png,jpeg}`])
+  .pipe(
+    webp({
+      quality: 70
+    })
+  )
+  .pipe(gulp.dest(config.dest))
+)
+
+gulp.task('clean', function () {
+    return del(config.dest);
+});
+
+gulp.task(
+    'default',
+    gulp.series(
+        'clean',
+        'copy:webp',
+        'copy:img'
+    )
+);
