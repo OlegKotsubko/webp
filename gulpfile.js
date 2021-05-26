4d865ba7b0a3e78b7aacde8a51f86b1ae19bec72
Oleg Kotsubko <olegkotsubko@gmail.com>
2021-05-26 15:10:40 +0300

    remove image optimization

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,13 @@
 const gulp = require("gulp");
-const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 const del = require('del');
-const clean = require('gulp-clean');
 
 const config = {
     src: './src',
     dest: './build'
 }
 
-gulp.task('copy:img', () => gulp
-  .src([`${config.src}/**/*.{jpg,png,jpeg}`])
-  .pipe(imagemin([
-    imagemin.mozjpeg({quality: 70, progressive: true}),
-    imagemin.optipng({optimizationLevel: 5}),
-  ]))
-  .pipe(gulp.dest(config.dest))
-);
-
-gulp.task('copy:webp', () => gulp
+gulp.task('webp', () => gulp
   .src([`${config.src}/**/*.{jpg,png,jpeg}`])
   .pipe(
     webp({
@@ -28,11 +17,6 @@ gulp.task('copy:webp', () => gulp
   .pipe(gulp.dest(config.dest))
 )
 
-gulp.task('clean:assets', () => {
-    return gulp.src(`${config.dest}/**/*.{jpg,png,jpeg}`, {read: false})
-      .pipe(clean());
-})
-
 gulp.task('clean', function () {
     return del(config.dest);
 });
@@ -41,8 +25,6 @@ gulp.task(
     'default',
     gulp.series(
         'clean',
-        'copy:webp',
-        'copy:img',
-        'clean:assets'
+        'webp',
     )
 );
